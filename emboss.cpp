63a5af4e820fde75768ba2913513f60e223e73bd
Daniel MK <danielmk98@hotmail.com>
2019-01-24 01:41:53 -0200

    O que já tinha

---
+++ emboss.cpp
@@ -0,0 +1,180 @@
+// g++ emboss.cpp `pkg-config --cflags --libs opencv` -std=c++11 && ./a.out test.png
+// ./a.out teste.png
+
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+typedef pair<int, int> ii;
+
+Mat img;
+int filter_size;
+Mat filter, bgn, nd;
+vector<ii> list;
+Mat res;
+
+int useful(int i, int j){
+	if(i == 0 || i == img.rows - 1 || j == 0 || j == img.cols - 1) return true;
+
+	return img.at<unsigned char>(i - 1 , j) == 0 || img.at<unsigned char>(i, j + 1) == 0 ||
+			img.at<unsigned char>(i + 1, j) == 0 || img.at<unsigned char>(i, j - 1) == 0;
+}
+
+int valid_pos(int i, int j){
+	return i >= 0 && i < img.rows && j >= 0 && j < img.cols;
+}
+
+int limitj(int j){
+	return min(img.cols - 1, max(0, j));
+}
+
+void make_list(){
+	int a, b;
+
+	for(int i = 0; i < filter.rows; i++){
+		a = -1;
+		for(int j = 0; j < filter.cols && a == -1; j++){
+			if(filter.at<bool>(i, j) == 1){
+				a = j - filter_size / 2;
+			}
+		}
+		
+		b = -1;
+		for(int j = filter.cols - 1; j >= 0 && b == -1; j--){
+			if(filter.at<bool>(i, j) == 1){
+				b = j - filter_size / 2;
+			}
+		}
+		
+		if(a != -1){
+			list.push_back(ii(a, b));
+		}
+	}
+}
+
+void fillbe(){
+	for(int i = 0; i < img.rows; i++){
+		for(int j = 0; j < img.cols; j++){
+			if(img.at<unsigned char>(i, j) != 0 && useful(i, j)){
+				for(int k = 0; k < list.size(); k++){
+					int l = i - filter_size / 2 + k;
+					int ce = limitj(j + list[k].first);
+					int cd = limitj(j + list[k].second);
+					if(valid_pos(l, ce)){
+						bgn.at<unsigned char>(l, ce) += 1;
+					}
+					if(valid_pos(l, cd)){
+						nd.at<unsigned char>(l, cd) += 1;
+					}
+				}
+			}
+		}
+	}
+}
+
+void make_res(){
+	for(int i = 0; i < res.rows; i++){
+		int balance = 0;
+		for(int j = 0; j < res.cols; j++){
+			balance += bgn.at<unsigned char>(i, j);
+			res.at<unsigned char>(i, j) = 255 * (balance > 0);
+			balance -= nd.at<unsigned char>(i, j);
+		}
+	}
+}
+
+void expand_white(int fs){
+	filter_size = fs;
+	filter = getStructuringElement(MORPH_ELLIPSE, Size(filter_size, filter_size));
+
+	list.clear();
+	make_list();
+	
+	bgn = Mat::zeros(img.rows, img.cols, CV_8U);
+	nd = Mat::zeros(img.rows, img.cols, CV_8U);
+	
+	fillbe();
+	
+	res = Mat(img.rows, img.cols, img.type());
+	make_res();
+	
+	add(img, res, img);
+}
+
+void invert(Mat vec){
+	bitwise_not(vec, vec);
+}
+
+int main(int argc, char** argv){
+/*
+	img = imread(argv[1], IMREAD_GRAYSCALE);
+	threshold(img, img, 0, 255, THRESH_BINARY + THRESH_OTSU);
+	
+	expand_white(29);
+	invert(img);
+	expand_white(9);
+	invert(img);
+	
+	imwrite("macho.png", img);
+	
+	img = imread(argv[1], IMREAD_GRAYSCALE);
+	threshold(img, img, 0, 255, THRESH_BINARY + THRESH_OTSU);
+	
+	invert(img);
+	expand_white(29);
+	invert(img);
+	expand_white(9);
+	invert(img);
+
+	imwrite("femea.png", img);
+	
+	img = imread("macho.png", IMREAD_GRAYSCALE);
+	invert(img);
+	
+	res = imread("femea.png", IMREAD_GRAYSCALE);
+	invert(res);
+	
+	add(img, res, img);
+	
+	imwrite("interseccao.png", img);
+*/
+	img = imread(argv[1], IMREAD_GRAYSCALE);
+	threshold(img, img, 0, 255, THRESH_BINARY + THRESH_OTSU);
+	
+	expand_white(37);
+	invert(img);
+	expand_white(13);
+	invert(img);
+	
+	Size sz1 = img.size();
+    Size sz2 = img.size();
+    Mat im3(sz1.height, sz1.width + sz2.width + 189, img.type());
+    Mat left(im3, Rect(0, 0, sz1.width, sz1.height));
+    img.copyTo(left);
+	
+	img = imread(argv[1], IMREAD_GRAYSCALE);
+	threshold(img, img, 0, 255, THRESH_BINARY + THRESH_OTSU);
+	
+	invert(img);
+	expand_white(37);
+	invert(img);
+	expand_white(13);
+	invert(img);
+	
+	flip(img, img, 1);
+
+	Mat right(im3, Rect(sz1.width + 189, 0, sz2.width, sz2.height));
+    img.copyTo(right);
+	
+	imwrite("res.png", im3);
+
+	return 0;
+}
---
+++ preemboss.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+import sys
+
+def RBGA2RBG(img):
+	height, width, channels = src.shape
+
+	if channels == 3:
+		return img
+
+	imgRBG = img[:, :, :3]
+
+	whiteBg = np.zeros((height, width, 3), np.uint8)
+	whiteBg[:] = (255, 255, 255)
+
+	imgAlpha = cv2.cvtColor(img[:, :, -1:], cv2.COLOR_GRAY2RGB)
+
+	imgRBG = imgRBG.astype(float)
+	whiteBg = whiteBg.astype(float)
+	imgAlpha = imgAlpha.astype(float) / 255
+
+	imgRBG = cv2.multiply(imgAlpha, imgRBG)
+	whiteBg = cv2.multiply(1.0 - imgAlpha, whiteBg)
+	imgRBG = cv2.add(imgRBG, whiteBg)
+	
+	return imgRBG
+
+def cm2pixels600dpi(cm):
+	return int(cm * 0.393701 * 600)
+
+def addPadding(img, size):
+	return cv2.copyMakeBorder(img, size, size, size, size, cv2.BORDER_CONSTANT, value = [255, 255, 255])
+
+src = cv2.imread(sys.argv[1], cv2.IMREAD_UNCHANGED)
+
+dst = RBGA2RBG(src)
+dst = addPadding(dst, cm2pixels600dpi(1))
+
+cv2.imwrite('test_pre.png', dst)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
